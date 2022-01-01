7256cd94fefedec7e0d731eba9c8b8683e0385bb
ritesh <riteshsingh.py@gmail.com>
2022-01-01 19:43:27 +0530

    added the add to cart button and created basic functionality of add to cart

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,24 @@
+import CartActionTypes from "./cart.types";
+
+const INITIAL_STATE = {
+  hidden: true,
+  cartItems: [],
+};
+
+const cartRedcuer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
+    case CartActionTypes.ADD_ITEMS:
+      return {
+        cartItems: [...state.cartItems, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default cartRedcuer;
